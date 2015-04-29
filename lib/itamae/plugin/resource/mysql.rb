75e0d6650a0ed20345944e00a356a6ddd63a9694
letusfly85 <letusfly95@gmail.com>
2015-04-29 22:04:38 +0900

    add actions

--- lib/itamae/plugin/resource/mysql.rb
+++ lib/itamae/plugin/resource/mysql.rb
@@ -1,10 +1,38 @@
-require "itamae/plugin/resource/mysql/version"
+#require "itamae/plugin/resource/mysql/version"
+require "itamae/resource/base"
 
 module Itamae
   module Plugin
     module Resource
-      module Mysql
-        # Your code goes here...
+      class Mysql < Itamae::Resource::Base
+        COMMAND = 'mysql'
+        define_attribute :action, :default, :start, :stop, :restart, :create_user, :drop_user
+
+        def set_current_attributes
+            #TODO
+        end
+
+
+        def action_start
+            #TODO
+        end
+
+        def action_stop
+            #TODO
+        end
+
+        def action_restart
+            #TODO
+        end
+
+        def action_create_user
+            #TODO
+        end
+
+        def action_drop_user
+            #TODO
+        end
+        
       end
     end
   end
--- lib/itamae/plugin/resource/mysql/version.rb
+++ lib/itamae/plugin/resource/mysql/version.rb
@@ -1,9 +1,9 @@
 module Itamae
-  module Plugin
-    module Resource
-      module Mysql
-        VERSION = "0.0.1"
-      end
+    module Plugin
+        module Resource
+            module Mysql
+                VERSION = "0.0.1"
+            end
+        end
     end
-  end
 end
