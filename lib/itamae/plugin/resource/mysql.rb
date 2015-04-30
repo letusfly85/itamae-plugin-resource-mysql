307280e2b3d5a545473f3ee2cb0e0e1514c89336
letusfly85 <letusfly95@gmail.com>
2015-04-30 13:57:21 +0900

    generate database command

--- lib/itamae/plugin/resource/mysql.rb
+++ lib/itamae/plugin/resource/mysql.rb
@@ -1,32 +1,64 @@
 #require "itamae/plugin/resource/mysql/version"
 require "itamae/resource/base"
+require "mysql2"
 
 module Itamae
   module Plugin
     module Resource
       class Mysql < Itamae::Resource::Base
         COMMAND = 'mysql'
-        define_attribute :action, :default, :start, :stop, :restart, :create_user, :drop_user
+        define_attribute :action, default: :start #, :stop, :restart, :create_user, :drop_user
+
+        define_attribute :loginuser, type: String, default_name: false
+        define_attribute :loginpass, type: String, default_name: false
+
+        define_attribute :username, type: String, default_name: false
+        define_attribute :password, type: String, default_name: false
+        define_attribute :database, type: String, default_name: false
+        define_attribute :host, type: String, default_name: false
+        define_attribute :port, type: String, default_name: false
 
         def set_current_attributes
-            #TODO
+            begin
+            @client = 
+                Mysql2::Client.new(:host => attributes.host,
+                                   :user => attributes.loginuser,
+                                   :password => attributes.loginpass,
+                                   :port => attributes.port)
+            rescue => e
+                Itamae::Logger::info e
+                raise e
+            end
         end
 
-
         def action_start
-            #TODO
+            unless current.exist
+                run_command(["/etc/init.d/mysql", "start"])
+            end
         end
 
         def action_stop
-            #TODO
+            unless current.exist
+                run_command(["/etc/init.d/mysql", "stop"])
+            end
         end
 
         def action_restart
-            #TODO
+            unless current.exist
+                run_command(["/etc/init.d/mysql", "restart"])
+            end
+        end
+
+        def action_create_database(options)
+            Itamae::Logger.info "create database..."
+            query = "create database #{attributes.database}"
+            Itamae::Logger.info query
+            results = @client.query(query)
         end
 
         def action_create_user
-            #TODO
+            query = %q{create user #{attributes.username} identified by '#{attributes.password}'}
+            results = @client.query(query)
         end
 
         def action_drop_user
