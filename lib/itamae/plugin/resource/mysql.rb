0fbddc5ddc83744c534b12e3711c6e8b1dd03eef
letusfly85 <letusfly95@gmail.com>
2015-05-01 09:18:30 +0900

    add create user actions

--- lib/itamae/plugin/resource/mysql.rb
+++ lib/itamae/plugin/resource/mysql.rb
@@ -12,6 +12,8 @@ module Itamae
         define_attribute :loginpass, type: String, default_name: false
 
         define_attribute :username, type: String, default_name: false
+        define_attribute :user_hosts, type: Array , default: []
+        define_attribute :user_priviliges, type: Array , default: []
         define_attribute :password, type: String, default_name: false
 
         define_attribute :database, type: String, default_name: false
@@ -46,8 +48,30 @@ module Itamae
         end
 
         def action_create_user
-            query = %q{create user #{attributes.username} identified by '#{attributes.password}'}
-            results = @client.query(query)
+            check_user_hosts
+
+            attributes.user_hosts.each do |host|
+                @query = "create user '#{attributes.username}'@'#{host}' identified by '#{attributes.password}'"
+
+                Itamae::Logger.info @query
+                results = @client.query(query)
+                Itamae::Logger.info "created '#{attributes.username}'@'#{host}'."
+            end
+        end
+
+        def action_grant_user
+            check_user_hosts
+            check_user_priviliges
+
+            attributes.user_hosts.each do |host|
+                attributes.user_priviliges.each do |privilige|
+                    @query = "grant #{privilige} on #{attributes.database}.* to '#{attributes.username}'@'#{host}'"
+
+                    Itamae::Logger.info @query
+                    results = @client.query(query)
+                    Itamae::Logger.info "granted #{privilige} to '#{attributes.username}'@'#{host}'."
+                end
+            end
         end
 
         def action_drop_user
@@ -57,7 +81,20 @@ module Itamae
             results = @client.query(query)
             Itamae::Logger.info "dropped #{attributes.username}."
         end
+
+        private
+
+        def check_user_hosts
+            if attributes.user_hosts.length == 0
+                raise "you should set host names in format ['host1', 'host2',...]"
+            end
+        end
         
+        def check_user_priviliges
+            if attributes.user_priviliges.length == 0
+                raise "you should set grant options in format ['option1', 'option2',...]"
+            end
+        end
       end
     end
   end
